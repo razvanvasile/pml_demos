f2fd6de4b7eb6293c0469161cab19f39775dddd5
Razvan <razvan.vasile07@gmail.com>
2017-04-25 13:36:29 +0100

    Add a method to print each family of pvs separately

--- scripts/elements_in_families.py
+++ scripts/elements_in_families.py
@@ -3,6 +3,12 @@ import mock
 sys.path.insert(0, '/home/cxa78676/pml/pml')
 import pml.load_csv
 
+
+def print_pv_names(elements, lattice):
+    for element in elements:
+        print element.get_pv_name('b1', 'readback'), "Position s =", lattice.get_s(element)
+
+
 def main():
     lattice = pml.load_csv.load('/home/cxa78676/pml/pml/test/data/VMX', 'VMX', mock.MagicMock())
     q1b = lattice.get_elements('Q1B')
@@ -11,19 +17,18 @@ def main():
     q2d = lattice.get_elements('Q2D')
     q3b = lattice.get_elements('Q3B')
     q3d = lattice.get_elements('Q3D')
-    for element in q1b:
-        print "Q1B pv:", element.get_pv_name('b1', 'readback'), "Position s =", lattice.get_s(element)
-    for element in q1d:
-        print "Q1D pv:", element.get_pv_name('b1', 'readback'), "Position s =", lattice.get_s(element)
-    for element in q2b:
-        print "Q2B pv:", element.get_pv_name('b1', 'readback'), "Position s =", lattice.get_s(element)
-    for element in q2d:
-        print "Q2D pv:", element.get_pv_name('b1', 'readback'), "Position s =", lattice.get_s(element)
-    for element in q3b:
-        print "Q3B pv:", element.get_pv_name('b1', 'readback'), "Position s =", lattice.get_s(element)
-    for element in q3d:
-        print "Q3D pv:", element.get_pv_name('b1', 'readback'), "Position s =", lattice.get_s(element)
-
+    print "Q1B pvs:"
+    print_pv_names(q1b, lattice)
+    print "Q1D pvs:"
+    print_pv_names(q1d, lattice)
+    print "Q2B pvs:"
+    print_pv_names(q2b, lattice)
+    print "Q2D pvs:"
+    print_pv_names(q2d, lattice)
+    print "Q3B pvs:"
+    print_pv_names(q3b, lattice)
+    print "Q3D pvs:"
+    print_pv_names(q3d, lattice)
 
 
 if __name__ == '__main__':
