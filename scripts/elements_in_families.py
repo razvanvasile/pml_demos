b6cdfdf678367b29fac8df14cb2b9c331bf13e8e
Razvan <razvan.vasile07@gmail.com>
2017-04-26 12:57:08 +0100

    Change output to match given file

--- scripts/elements_in_families.py
+++ scripts/elements_in_families.py
@@ -3,12 +3,11 @@ import mock
 sys.path.insert(0, '/home/cxa78676/pml/pml')
 import pml.load_csv
 
-
 def print_pv_names(elements, lattice):
     with open('elements_in_families.txt', 'a') as out_file:
         for element in elements:
-            out_file.write("{}\n".format(element.get_pv_name('b1', 'readback')))
-
+            pv_name = element.get_pv_name('b1', 'readback').split(':')[0]
+            out_file.write("{}\n".format(pv_name))
 
 def main():
     lattice = pml.load_csv.load('/home/cxa78676/pml/pml/test/data/VMX', 'VMX', mock.MagicMock())
@@ -18,12 +17,12 @@ def main():
     q2d = lattice.get_elements('Q2D')
     q3b = lattice.get_elements('Q3B')
     q3d = lattice.get_elements('Q3D')
-    print_pv_names(q1b, lattice)
     print_pv_names(q1d, lattice)
-    print_pv_names(q2b, lattice)
     print_pv_names(q2d, lattice)
-    print_pv_names(q3b, lattice)
     print_pv_names(q3d, lattice)
+    print_pv_names(q3b, lattice)
+    print_pv_names(q2b, lattice)
+    print_pv_names(q1b, lattice)
 
 
 if __name__ == '__main__':
